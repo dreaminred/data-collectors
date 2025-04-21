b7558d4a4a404674e260351cbeaa6016608c40ac
Armin Hatami <43918725+dreaminred@users.noreply.github.com>
2025-04-20 18:15:26 -0700

    Started working on data cleaning pipeline.

--- barstool-pizza.py
+++ barstool-pizza.py
@@ -2,6 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+def process_scraped_data(data: pd.DataFrame):
+    '''
+    Process the scraped data to extract latitude and longitude
+    '''
+    data = pd.read_csv('names-locations.csv')
+    # Splitting City,State column into two separate columns using comma as the delimiter
+    # Loading us cities data
+    us_cities = pd.read_csv('uscities.csv')
+    us_cities = us_cities[['city', 'state_id', 'lat', 'lng']]
+    us_cities['cityName'] = us_cities['city'] + ', ' + us_cities['state_id']
+
+    # Merging the scraped data with the US cities data to get latitude and longitude
+    data = pd.merge(data, us_cities, left_on='Location', right_on='cityName', how='left')
+
+    # Print the number of missing values in the latitude and longitude columns
+    print("Missing values in latitude and longitude columns:")
+    print(data['lat'].isnull().sum())
+    print(data['lng'].isnull().sum())
+    # Print 10 random rows with missing lat long values
+    print("Random rows with missing latitude and longitude values:")
+    print(data[data['lat'].isnull() | data['lng'].isnull()].sample(10)) 
+    # Groupy by City and State, print the top 10 visited cities
+    grouped = data.groupby(['cityName']).size().reset_index(name='Occurrences')
+    grouped = grouped.sort_values(by='Occurrences', ascending=False)
+    print(grouped.head(20))
+    return data
+
+def getAPIurl(Lat: float,Lon: float) -> str:
+    '''
+    Returns API url using lat/long search
+    '''
+    return("https://one-bite-api.barstoolsports.com/venue?dave=1&lat=" + str(Lat) + "&lon=" + str(Lon))
+
 def scrape_data():
 
     pageNumbers = range(1,66)
@@ -15,6 +48,8 @@ def scrape_data():
     # Storing the data in a pandas DataFrame and saving it to a CSV file
     df = pd.DataFrame(data_rows)
     df.to_csv('names-locations.csv', index=False)
+
+    return df
 
 
 def get_page_data(pageNumber):
@@ -40,7 +75,8 @@ def get_page_data(pageNumber):
     return data_rows
 
 def main():
-    scrape_data()
+    #scrape_data()
+    process_scraped_data(pd.DataFrame())
     return 0
 
 
